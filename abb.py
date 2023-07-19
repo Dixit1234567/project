65a984d55305985c22ca6fe68a8912611123efa6
Dixit1234567 <k97161204@gmail.com>
2023-07-19 12:30:29 +0530

    1st change

--- abb.py
+++ abb.py
@@ -1,2 +1,17 @@
-print("ABD")
-print("hi")
+import numpy as np
+
+# Create a NumPy array
+numbers = np.array([1, 2, 3, 4, 5])
+
+# Perform some common NumPy functions
+mean_value = np.mean(numbers)
+median_value = np.median(numbers)
+standard_deviation = np.std(numbers)
+variance = np.var(numbers)
+
+# Print the results
+print("Original array:", numbers)
+print("Mean:", mean_value)
+print("Median:", median_value)
+print("Standard Deviation:", standard_deviation)
+print("Variance:", variance)
